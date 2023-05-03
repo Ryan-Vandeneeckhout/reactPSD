ee4bf8fb857dab57cd818aa99a7f1b9af9e813e2
Ryan-Vandeneeckhout <exigoan131@gmail.com>
2023-05-03 01:01:44 -0400

    margin fix

--- src/components/blog/blogMapItem.jsx
+++ src/components/blog/blogMapItem.jsx
@@ -29,19 +29,19 @@ export const blogMapItem = [
 
     posts: [
       {
-        date: "April 18th 2022",
+        date: "April 18 2022",
         pcontent:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem vel delectus ipsam harum! Molestias aperiam commodi soluta, quidem, quaerat quos, quia rem neque explicabo beatae dolore animi exercitationem obcaecati doloribus!",
         image: "./assets/images/galleryPictures/gallery8.jpg",
       },
       {
-        date: "April 14th 2022",
+        date: "April 14 2022",
         pcontent:
           "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam voluptatibus blanditiis quidem sunt neque asperiores voluptas eum magnam pariatur quis libero vero hic, facere corporis illum reiciendis similique. Alias, facere!",
         image: "./assets/images/galleryPictures/gallery6.jpg",
       },
       {
-        date: "April 1th 2022",
+        date: "April 1 2022",
         pcontent:
           "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat obcaecati minus aut ea maxime ipsam dolor, tenetur omnis, laboriosam nostrum vel reiciendis quia non nulla suscipit alias consequatur voluptatem hic!",
         image: "./assets/images/galleryPictures/gallery5.jpg",
